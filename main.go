17178f619dfec22dc668918c82d59762fe00b84e
henry <henry@henrys-air.lan>
2024-07-05 15:06:44 -0400

    render some html

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func h1(w http.ResponseWriter, r *http.Request) {
+	templ := template.Must(template.ParseFiles("index.html"))
+	templ.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("hej!")
+	http.HandleFunc("/", h1)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Printf("failed to open http server %v", err)
+	}
 }
